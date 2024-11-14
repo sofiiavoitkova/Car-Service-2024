2489bdd5147df990ce3068eba7aeacb616a7052f
Nara <mills.nargiz@gmail.com>
2024-11-14 06:53:26 +0300

    Updated main section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
 import './App.css';
 
 function App() {
