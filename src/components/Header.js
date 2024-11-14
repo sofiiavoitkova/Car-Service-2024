cb55bd53d0d6bd98455eb5999417b38ac1d1def4
Nara <mills.nargiz@gmail.com>
2024-11-14 07:18:13 +0300

    Working Website #1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Header.css';
+import logo from '../assets/logo.png';
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/path/to/logo.png" alt="Elecar Logo" /> {/* Replace with your logo image path */}
-        <span>Elecar</span>
+        <img src={logo} alt="Roadready Logo" />
+        <span>Road Ready</span>
       </div>
       <nav>
         <a href="#home" className="active">Home</a>
         <a href="#about">About</a>
-        <a href="#popular">Popular</a>
-        <a href="#featured">Featured</a>
+        <a href="#popular">Workshops</a>
+        <a href="#featured">Featured Brands</a>
       </nav>
     </header>
   );
